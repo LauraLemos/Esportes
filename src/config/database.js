1f3daf9da68bdfa16de382b4a552c9da2d3d4a1b
Laura Lemos <laurinhalemos78@gmail.com>
2021-04-25 16:24:53 -0300

    teste1

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: "postgres",
+    host: "localhost",
+    port: "5433",
+    username: "postgres",
+    password: "30012003",
+    database: "esportes" ,
+    define:{
+        timestemps:true
+    }
+}
