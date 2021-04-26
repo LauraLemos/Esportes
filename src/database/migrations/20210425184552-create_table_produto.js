d3ae7520febb1c154bb79f961e3bbe42a697ae5f
Laura Lemos <laurinhalemos78@gmail.com>
2021-04-26 00:16:28 -0300

    add 5 - parte um

--- src/database/migrations/20210425184552-create_table_produto.js
+++ src/database/migrations/20210425184552-create_table_produto.js
@@ -29,7 +29,7 @@ module.exports = {
         allowNull: false
       },
 
-      updatedAt :{
+      updatedAt:{
         type: Sequelize.DATE,
         allowNull: false
       }
---
+++ src/model/Produto.js
@@ -0,0 +1,14 @@
+const {Model, DataTypes} = require('sequelize')
+
+class Produto extends Model {
+    static init(sequelize){
+        super.init({
+            nome: DataTypes.STRING,
+            valor: DataTypes.DOUBLE,
+            tipo: DataTypes.STRING
+            
+        },{sequelize,freezeTableName:true,})
+    }
+}
+
+module.exports = Produto;
